df24a1b41e851ebec2765a39a4af57f033b682f7
HajReza <a.rz.karbasi@gmail.com>
2019-09-05 11:06:25 +0430

    first algoritm -> similar to best client

--- src/YourClient.py
+++ src/YourClient.py
@@ -9,26 +9,39 @@ def get_action(world: World):
     next_head.append(head_pos + Vector2D(0, 1))
     next_head.append(head_pos + Vector2D(-1, 0))
     next_head.append(head_pos + Vector2D(0, -1))
-    next_head_prince = [0, 0, 0, 0]
+    next_head_price = [0, 0, 0, 0]
     actions = ['d', 'r', 'u', 'l']
+
+    obstacle_1 = []
+    obstacle_2 = []
+
+    obstacle_1 += world.get_walls()
+    for s in world.snakes:
+        snake_temp = world.snakes[s]
+        snake_temp_head = world.snakes[s].get_head()
+
+        obstacle_1 += snake_temp.get_body()
+
+        if snake_temp_head is not head_pos:
+            obstacle_2.append(snake_temp_head + Vector2D(1, 0))
+            obstacle_2.append(snake_temp_head + Vector2D(0, 1))
+            obstacle_2.append(snake_temp_head + Vector2D(-1, 0))
+            obstacle_2.append(snake_temp_head + Vector2D(0, -1))
 
     h_number = -1
     for h in next_head:
         h_number += 1
-        accident = False
-        for s in world.snakes:
-            if h in world.snakes[s].get_body():
-                accident = True
-                next_head_prince[h_number] = 100
-                break
-        if accident:
-            print(h, 'snake')
-        elif h in world.get_walls():
-            accident = True
-            print(h, 'wall')
-            next_head_prince[h_number] = 100
+        
+        if h in obstacle_1:
+            next_head_price[h_number] += 60
+        if h in obstacle_2:
+            next_head_price[h_number] += 20*obstacle_2.count(h)
+        
+        next_head_price[h_number]+=world.goal_position.dist(h)
 
-    mini = min(next_head_prince)
+    print(next_head_price)
+    print(actions)
+    mini = min(next_head_price)
     for h_number in range(4):
-        if next_head_prince[h_number] == mini:
+        if next_head_price[h_number] == mini:
             return actions[h_number]
