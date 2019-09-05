9bab8ea6531a055076e1f7175a3d081b306d9950
HajReza <a.rz.karbasi@gmail.com>
2019-09-05 17:45:37 +0430

    improved A* but still hang is the first problem

--- src/YourClient.py
+++ src/YourClient.py
@@ -51,11 +51,12 @@ def aStar(start: Node,  goal: Vector2D,  obs: list):
     # While the open set is not empty
     while openset:
         counter += 1
-        if counter > 150:
+        if counter > 100:
             return []
 
         # Find the item in the open set with the lowest G + H score
-        current = min(openset, key=lambda o: o.G + o.H)
+        current = min(openset, key=lambda o: 2*o.G +
+                      3*o.H)                 # resideG
 
         # If it is the item we want, retrace the path and return it
         if current.point == goal:
@@ -102,8 +103,30 @@ def aStar(start: Node,  goal: Vector2D,  obs: list):
     return []
 
 
+def my_fast_selection(next_head: list, goal: Vector2D, obstacle_1: list, obstacle_2: list):
+
+    next_head_price = [0,0,0,0]
+    actions = ['d', 'r', 'u', 'l']
+    h_number = -1
+    for h in next_head:
+        h_number += 1
+
+        if h in obstacle_1:
+            next_head_price[h_number] += 60
+        if h in obstacle_2:
+            next_head_price[h_number] += 15*obstacle_2.count(h)
+        next_head_price[h_number] += goal.dist(h)
+
+    mini = min(next_head_price)
+    for h_number in range(4):
+        if next_head_price[h_number] == mini:
+            return actions[h_number]
+
+
 def get_action(world: World):
+    goal = world.goal_position
     head_pos = world.get_self().get_head()
+
     next_head = []
     next_head.append(head_pos + Vector2D(1, 0))
     next_head.append(head_pos + Vector2D(0, 1))
@@ -112,11 +135,23 @@ def get_action(world: World):
     next_head_price = [0, 0, 0, 0]
     actions = ['d', 'r', 'u', 'l']
 
+    if goal in next_head:
+        for i in range(4):
+            if next_head[i] is goal:
+                return actions[i]
+
     obstacle_1 = []
     obstacle_2 = []
 
     obstacle_1 += world.get_walls()
+
+    min_dist = 1000
+    my_dist = goal.dist(head_pos)
     for s in world.snakes:
+        t = world.snakes[s].get_head().dist(goal)
+        if min_dist > t:
+            min_dist = t
+
         snake_temp = world.snakes[s]
         snake_temp_head = world.snakes[s].get_head()
 
@@ -128,7 +163,14 @@ def get_action(world: World):
             obstacle_2.append(snake_temp_head + Vector2D(-1, 0))
             obstacle_2.append(snake_temp_head + Vector2D(0, -1))
 
-    a_star = aStar(Node(head_pos), world.goal_position, obstacle_1)
+    if 3*min_dist < my_dist:
+        # fast selection to center
+        return my_fast_selection(next_head, Vector2D(14,14), obstacle_1, obstacle_2)
+    if my_dist > 25:
+        # fast selection to goal
+        return my_fast_selection(next_head, goal, obstacle_1, obstacle_2)
+
+    a_star = aStar(Node(head_pos), goal, obstacle_1)
 
     h_number = -1
     for h in next_head:
@@ -137,9 +179,9 @@ def get_action(world: World):
         if h in obstacle_1:
             next_head_price[h_number] += 60
         if h in obstacle_2:
-            next_head_price[h_number] += 20*obstacle_2.count(h)
+            next_head_price[h_number] += 15*obstacle_2.count(h)
         if h in a_star:
-            next_head_price[h_number] -= 15
+            next_head_price[h_number] -= 20
         # next_head_price[h_number]+=world.goal_position.dist(h)
 
     print(next_head_price)
